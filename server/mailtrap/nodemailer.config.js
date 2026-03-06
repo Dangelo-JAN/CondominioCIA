d21d25e6138de83be4c0d64c81c0264f2df868a1
Dangelo Arrivillaga <dangeloarrivillaga@gmail.com>
2026-03-06 18:39:30 -0400

    fix mailsender v5

--- server/mailtrap/nodemailer.config.js
+++ server/mailtrap/nodemailer.config.js
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const transporter = nodemailer.createTransport({
+export const Emailclient = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: process.env.EMAIL_USER,
