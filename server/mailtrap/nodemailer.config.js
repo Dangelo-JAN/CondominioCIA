b1f8b80d7ec436488f75bf97836c1c5a49b29e20
Dangelo Arrivillaga <dangeloarrivillaga@gmail.com>
2026-03-15 18:04:49 -0400

    chore: switch mailtrap by nodemailer v3

--- server/mailtrap/nodemailer.config.js
+++ server/mailtrap/nodemailer.config.js
@@ -3,11 +3,17 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+    tls: {
+        rejectUnauthorized: false
+    }
 })
 
 export const sender = {
